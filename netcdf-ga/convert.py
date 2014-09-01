1f9c38a82c41c2ba7da934aa8c7a59d289765b47
Terry Rankine <terry.rankine@csiro.au>
2014-09-01 22:28:41 +0800

    * changed detection algorithm

--- netcdf-ga/convert.py
+++ netcdf-ga/convert.py
@@ -12,14 +12,14 @@ gdal_command = ['gdal_translate', \
                 '-of', 'netCDF', \
                 '-co', 'FORMAT=NC4C', \
                 '-co', 'COMPRESS=DEFLATE', ]
-'''                
+'''
 ncrename -v Band1,elevation busselton-final.nc
 ncatted -h -O -a long_name,elevation,o,c,elevation busselton-final.nc
 ncatted -h -O -a  positive,elevation,a,c,"up" busselton-final.nc
 ncatted -h -O -a  units,elevation,a,c,"http://www.opengis.net/def/uom/OGC/1.0/metre" busselton-final.nc
 ncatted -h -O -a  vrs,elevation,a,c,"http://www.opengis.net/def/crs/epsg/0/5711" busselton-final.nc
 '''
-inPlaceCleanup = [ 
+inPlaceCleanup = [
             ['ncrename', '-v', 'Band1,elevation', ], \
             ['ncatted', '-h', '-O', '-a', 'long_name,elevation,o,c,elevation', ], \
             ['ncatted', '-h', '-O', '-a', 'positive,elevation,a,c,"up"', ], \
@@ -28,16 +28,15 @@ inPlaceCleanup = [
           ]
 
 
-outputdir = "/projects/p36/thredds-bathemirty-dump"
+outputdir = "/g/data1/rr1/conversions"
 
 for root, subFolders, files in os.walk("/g/data1/rr1/"):
-    if 'w001001.adf' in files:
-        inputfile = root+ '/w001001.adf'
-        prettyname = root.replace("/g/data1/rr1/","").replace("/","_")
+    if 'prj.adf' in files:
+        inputfile = root + os.sep + 'prj.adf'
+        prettyname = root.replace("/g/data1/rr1/","").replace("/","_").replace(" ", "_") + '.nc'
         outputfullname = outputdir + os.path.sep + prettyname
         print gdal_command + [inputfile, outputfullname]
         subprocess.check_call(gdal_command + [inputfile, outputfullname])
         for operation in inPlaceCleanup:
             print operation + [outputfullname]
             subprocess.check_call(operation + [outputfullname])
-        
