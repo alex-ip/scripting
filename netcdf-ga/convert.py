33ac6fe9f8524c5b34b38ab22ad1b17a68ffa4d9
Terry Rankine <terry.rankine@gmail.com>
2014-11-19 14:34:56 +0800

    updated conversions

--- netcdf-ga/convert.py
+++ netcdf-ga/convert.py
@@ -11,28 +11,30 @@ import os, subprocess
 gdal_command = ['gdal_translate', \
                 '-of', 'netCDF', \
                 '-co', 'FORMAT=NC4C', \
-                '-co', 'COMPRESS=DEFLATE', ]
-'''                
+                '-co', 'COMPRESS=DEFLATE',
+                '-co', 'WRITE_BOTTOMUP=YES', ]
+'''
 ncrename -v Band1,elevation busselton-final.nc
 ncatted -h -O -a long_name,elevation,o,c,elevation busselton-final.nc
 ncatted -h -O -a  positive,elevation,a,c,"up" busselton-final.nc
 ncatted -h -O -a  units,elevation,a,c,"http://www.opengis.net/def/uom/OGC/1.0/metre" busselton-final.nc
 ncatted -h -O -a  vrs,elevation,a,c,"http://www.opengis.net/def/crs/epsg/0/5711" busselton-final.nc
 '''
-inPlaceCleanup = [ 
+inPlaceCleanup = [
             ['ncrename', '-v', 'Band1,elevation', ], \
             ['ncatted', '-h', '-O', '-a', 'long_name,elevation,o,c,elevation', ], \
             ['ncatted', '-h', '-O', '-a', 'positive,elevation,a,c,"up"', ], \
-            ['ncatted', '-h', '-O', '-a', 'units,elevation,a,c,"http://www.opengis.net/def/uom/OGC/1.0/metre"', ], \
-            ['ncatted', '-h', '-O', '-a', 'vrs,elevation,a,c,"http://www.opengis.net/def/crs/epsg/0/5711"', ], \
+            ['ncatted', '-h', '-O', '-a', 'units-uri,elevation,a,c,"http://www.opengis.net/def/uom/OGC/1.0/metre"', ], \
+            ['ncatted', '-h', '-O', '-a', 'units-uri,elevation,a,c,"metre"', ], \
+            ['ncatted', '-h', '-O', '-a', 'vrs-uri,elevation,a,c,"http://www.opengis.net/def/crs/EPSG/0/5773"', ], \
+            ['ncatted', '-h', '-O', '-a', 'vrs,elevation,a,c,"EGM96"', ], \
           ]
 
+outputdir = "/g/data1/rr1/conversions/newconvert"
 
-outputdir = "/projects/p36/thredds-bathemirty-dump"
-
-for root, subFolders, files in os.walk("/g/data1/rr1/"):
-    if 'w001001.adf' in files:
-        inputfile = root+ '/w001001.adf'
+for root, subFolders, files in os.walk("/g/data1/rr1/Elevation/1secSRTM_DEMs_v1.0/"):
+    if 'prj.adf' in files:
+        inputfile = root
         prettyname = root.replace("/g/data1/rr1/","").replace("/","_")
         outputfullname = outputdir + os.path.sep + prettyname
         print gdal_command + [inputfile, outputfullname]
@@ -40,4 +42,4 @@ for root, subFolders, files in os.walk("/g/data1/rr1/"):
         for operation in inPlaceCleanup:
             print operation + [outputfullname]
             subprocess.check_call(operation + [outputfullname])
-        
+        subprocess.check_call(['nccopy', '-d', '2', '-c', 'lat/128,lon/128', outputfullname,  outputfullname+'.128-128.nc'])
